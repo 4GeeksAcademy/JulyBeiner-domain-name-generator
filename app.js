583eb4b5e9e193f0dc84699f04d8c59410ff4465
July Beiner <july.jarabeiner@gmail.com>
2026-05-17 15:28:30 +0000

    domain name generator

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.io', '.us'];
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+            for (let e = 0; e < extensions.length; e++) {
+                console.log(pronoun[i] + adj[j] + noun[k] + extensions[e]);
+            }
+        }
+    }
+}
